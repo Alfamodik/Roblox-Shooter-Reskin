2beb1c830c803c936ec9df80998e015ebefc4218
DESKTOP-HI59H2R\Alfamod <Salnikovdmitry.2006@gmail.com>
2026-01-30 18:26:54 +0500

    Added YG2 localization

--- Assets/Scripts/InfoUI/UIKilledEnemy.cs
+++ Assets/Scripts/InfoUI/UIKilledEnemy.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using YG.LanguageLegacy;
 
 [RequireComponent(typeof(TMP_Text))]
 public class UIKilledEnemy : MonoBehaviour
@@ -11,7 +12,7 @@ public class UIKilledEnemy : MonoBehaviour
 
     [Space]
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private SetLanguage _language;
+    [SerializeField] private LanguageYG _language;
 
     private int _score = 0;
     private string _startText;
@@ -21,21 +22,32 @@ public class UIKilledEnemy : MonoBehaviour
 
     private Vector3 _startScale;
     private RectTransform _rectTransform;
+    private bool _isInitialized;
 
     public void Initialize(IScoreNotifier scoreNotifier)
     {
-        _language.Initialize();
-
         _TMP_Text = GetComponent<TMP_Text>();
+        _rectTransform = GetComponent<RectTransform>();
 
         _scoreNotifier = scoreNotifier;
         _scoreNotifier.OnScoreChanged += OnScoreChange;
 
         _startText = _TMP_Text.text;
-        _TMP_Text.text = $"{_startText}{_score}/{_scoreNotifier.PointToRankUpgrade}";
-
-        _rectTransform = GetComponent<RectTransform>();
         _startScale = _rectTransform.localScale;
+
+        StartCoroutine(InitializeCoroutine());
+    }
+
+    private IEnumerator InitializeCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        Destroy(_language);
+        
+        if (_isInitialized) 
+            yield break;
+
+        _TMP_Text.text = $"{_startText}{_score}/{_scoreNotifier.PointToRankUpgrade}";
     }
 
     private void OnDestroy()
@@ -46,6 +58,12 @@ public class UIKilledEnemy : MonoBehaviour
 
     private void OnScoreChange(int score)
     {
+        if (!_isInitialized)
+        {
+            _isInitialized = true;
+            Destroy(_language);
+        }
+
         _score = score;
         _TMP_Text.text = $"{_startText}{_score}/{_scoreNotifier.PointToRankUpgrade}";
 
@@ -59,7 +77,7 @@ public class UIKilledEnemy : MonoBehaviour
         DoTween.ChangeScale(_rectTransform, new Vector3(_maxScale, _maxScale, _maxScale),
             _animationDuration, Ease.Linear, UpdateType.Normal, true);
 
-        //Игнорировать при первом Invoke
+        //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ Invoke
         _audioSource?.Play();
         yield return new WaitForSeconds(_animationDuration);
 
